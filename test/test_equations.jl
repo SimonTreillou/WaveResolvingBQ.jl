6f080600c85ac591a2a77907997b7636987f5c86
SimonTreillou <simontreillou@orange.fr>
2024-10-09 18:44:39 +0200

    update test

--- test/test_equations.jl
+++ test/test_equations.jl
@@ -3,19 +3,33 @@ using WaveResolvingBQ
 
 @testset "Window with Euler+UP1 at CFL=1" begin     
     # Parameters
-    P=WaveResolvingBQ.SimulationParameters(Δt=0.1,T=5000.0,Δx=0.1,Lx=100.0,c=1.0)
+    P=WaveResolvingBQ.SimulationParameters(Δt=0.01,T=4.0)
+    G=WaveResolvingBQ.Grid(Lx=6,Δx=0.01)
 
     # Init
-    η_init = window(P.x,0.1,1.0)
+    C_init = zeros(G.Nx)
+    for i in 1:G.Nx
+        if (G.x[i]>0.5) & (G.x[i]<1.0)
+            C_init[i]=1.0
+        end
+    end
 
     # Model
-    M=WaveResolvingBQ.Advection()
-    # Timestepping
+    M=WaveResolvingBQ.Advection(1.0)
+    B=WaveResolvingBQ.Null(2)
+
+    # Euler+UP1
     T=WaveResolvingBQ.Euler()
-    # Spatial scheme
     S=WaveResolvingBQ.UP1()
+    C_UP1,TV=  WaveResolvingBQ.run(M,T,S,P,B,G,C_init)
 
-    η_end,TV= WaveResolvingBQ.run(M,T,S,P,η_init)
+    # Solution
+    C_sol = zeros(G.Nx)
+    for i in 1:G.Nx
+        if (G.x[i]>4.5) & (G.x[i]<5.0)
+            C_sol[i]=1.0
+        end
+    end
 
-    @test η_init ≈ η_end
+    @test C_sol ≈ C_UP1
 end
